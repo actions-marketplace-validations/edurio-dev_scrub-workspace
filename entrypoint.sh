a026c535d9f4401cdaea75a2d7e88ba3f9429701
Ilmārs <91464474+ilmars-edurio@users.noreply.github.com>
2022-06-28 12:53:06 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -e
+
 shopt -s dotglob
+
 REPO_NAME=${GITHUB_REPOSITORY#*/}
+
 find /__w/${REPO_NAME}/${REPO_NAME}/. -name . -o -prune -exec rm -rf -- {} + || true
+find /runner/_work/${REPO_NAME}/${REPO_NAME}/. -name . -o -prune -exec rm -rf -- {} + || true
